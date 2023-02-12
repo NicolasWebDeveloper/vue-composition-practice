46772f4f2a926da61fb25ccfe2f24a2c7d0962b9
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-02-12 20:26:35 +0100

    Initial 🎉

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  {{ fullName }}
+  <input type="text" @input="setFName" />
+  <input type="text" @input="setLName" />
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  setup() {
+    const fName = ref('Peter');
+    const lName = ref('Maier');
+
+    function setFName(e) {
+      fName.value = e.target.value;
+    }
+
+    function setLName(e) {
+      lName.value = e.target.value;
+    }
+
+    const fullName = computed(function () {
+      return fName.value + ' ' + lName.value;
+    });
+
+    return { fName, lName, fullName, setLName, setFName };
+  },
+};
+</script>
